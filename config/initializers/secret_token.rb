a9c4c6337a52c6fdbc2aa9c7a22a6eb8e670d460
Jay Phillips <jicksta+git@gmail.com>
2011-03-28 13:30:49 -0700

    New Rails 3.0.5 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VeganData::Application.config.secret_token = '6b1ad88176228d52d8abaee203cd94ed41be1d6fbc88de8f0f2eeef131cfc5a350e6064384be79ca8886230f46bfac1a67e4a05b5e55a61ef1baaab8d4e191e3'
